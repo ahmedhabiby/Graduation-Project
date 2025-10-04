11c48b2ba78ff003c1af699491aa1400796b3c31
ahmedhabiby <116781473+ahmedhabiby@users.noreply.github.com>
2025-10-04 13:04:10 +0300

    second commit

--- ExplurerHub/src/main/java/com/example/explurerhub/Controllers/TransferController.java
+++ ExplurerHub/src/main/java/com/example/explurerhub/Controllers/TransferController.java
@@ -6,59 +6,108 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TransferController {
     @GetMapping("/about")
-    public String showAbout(){
+    public String showAbout() {
         return "about";
     }
+
     @GetMapping("/where-to-go")
-    public String showWhereToGo(){
+    public String showWhereToGo() {
         return "where-to-go";
     }
+
     @GetMapping("/what-to-do")
-    public String showWhatToDo(){
+    public String showWhatToDo() {
         return "what-to-do";
     }
+
     @GetMapping("/whats-on")
-    public String showWhatsOn(){
+    public String showWhatsOn() {
         return "whats-on";
     }
+
     @GetMapping("/useful-info")
-    public String showUsefulInfo(){
+    public String showUsefulInfo() {
         return "useful-info";
     }
+
     @GetMapping("/cities")
-    public String showCities(){
+    public String showCities() {
         return "cities";
     }
+
     @GetMapping("/nile")
-    public String showNile(){
+    public String showNile() {
         return "nile";
     }
+
     @GetMapping("/red-sea")
-    public String showRedSea(){
+    public String showRedSea() {
         return "red-sea";
     }
+
     @GetMapping("/med")
-    public String showMed(){
+    public String showMed() {
         return "med";
     }
+
     @GetMapping("/deserts")
-    public String showDeserts(){
+    public String showDeserts() {
         return "deserts";
     }
+
     @GetMapping("/sun-sea")
-    public String showSunSea(){
+    public String showSunSea() {
         return "sun-and-sea";
     }
-    @GetMapping("/archaeological-sites")
-    public String showArchaeologicalSites(){
-        return "archaeological-sites";
+
+    @GetMapping("/abu-simble")
+    public String showAbuSimble() {
+        return "abu-simble";
     }
-    @GetMapping("/adventure-outdoor")
-    public String showAdventureOutdoor(){
-        return "adventure-outdoor";
+    @GetMapping("/old-alexandria")
+    public String showOldAlexandria() {
+        return "alexandria-old";
     }
-    @GetMapping("/arts-culture")
-    public String showArtsCulture(){
-        return "arts-culture";
+    @GetMapping("/aswan-city")
+    public String showAswanCity() {
+        return "aswan-city";
     }
+    @GetMapping("/aswan-food")
+    public String showAswanFood() {
+        return "aswan-food";
+    }
+    @GetMapping("/aswan-mosques")
+    public String showAswanMosques() {
+        return "aswan-mosques";
+    }
+    @GetMapping("/aswan-museums")
+    public String showAswanMuseums() {
+        return "aswan-museums";
+    }
+    @GetMapping("/old-aswan")
+    public String showOldAswan() {
+        return "aswan-old";
+    }
+    @GetMapping("/bahariya")
+    public String showBahariya() {
+        return "bahariya-city";
+    }
+    @GetMapping("/bahariya-hotels")
+    public String showBahariyaHotels() {
+        return "bahariya-hotels";
+    }
+    @GetMapping("/bahariya-transport")
+    public String showBahariyaTransport() {
+        return "bahariya-transport";
+    }
+    @GetMapping("/bibliotheca")
+    public String showBibliotheca() {
+        return "bibliotheca";
+    }
+    @GetMapping("/cairo-food")
+    public String showCairoFood() {
+        return "cairo-food";
+    }
+
 }
+
